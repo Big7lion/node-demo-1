d8f87aba303953c7116485f3545a2f858ea024bb
River <moonquakes7@gmail.com>
2021-02-24 22:22:56 +0800

    更新

--- server.js
+++ server.js
@@ -25,9 +25,8 @@ var server = http.createServer(function(request, response){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
-    <!DOCTYPE html>
-    <link rel="stylesheet" href="/style.css" >
-    <h1>你好</h1>
+      <link rel="stylesheet" href="/style.css" >
+      <h1>你好</h1>
     `)
     response.end()
   } else if(path === '/style.css'){
