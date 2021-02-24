1b9918cf46b51b825f41de2151c90a2d568270cf
River <moonquakes7@gmail.com>
2021-02-24 21:56:39 +0800

    修改

--- server.js
+++ server.js
@@ -24,12 +24,16 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`h1{color: red}`)
+    response.write(`
+    <!DOCTYPE html>
+    <link rel="stylesheet" href="/style.css" >
+    <h1>你好</h1>
+    `)
     response.end()
-  } else if(path === '/x'){
-    response.statusCode = 404
+  } else if(path === '/style.css'){
+    response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`你访问的页面不存在`)
+    response.write(`h1{color: red;}`)
     response.end()
   } else {
     response.statusCode = 404
